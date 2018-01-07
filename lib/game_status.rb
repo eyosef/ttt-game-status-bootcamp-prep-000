fbb8033da6d447a1e678b9cc93f744a2bc1fcab8
Eden Mesfun <emesfun@gmail.com>
2018-01-07 14:55:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,14 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |array|
+    if board[0..2] != array || board[3..5] != array || board[6..8] != array 
+      false
+    elsif board[0..2].to_ary == WIN_COMBINATIONS.include?([0,1,2]).to_ary
+      true 
+    end
+  end
+end 
